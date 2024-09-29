dede3dceab96ab46bca9d83d351b292dd761276c
Forest0923 <masahirom0923@gmail.com>
2024-09-29 22:20:36 +0900

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::fs;
+
+#[tauri::command]
+fn read_file(path: String) -> Result<String, String> {
+    fs::read_to_string(path).map_err(|err| err.to_string())
+}
+
+#[tauri::command]
+fn write_file(path: String, content: String) -> Result<(), String> {
+    fs::write(path, content).map_err(|err| err.to_string())
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![read_file, write_file])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
